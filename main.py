efb9524d7ba06a6d095f18b7af0f0c12e201f747
Boxfort <40208539@live.napier.ac.uk>
2017-09-02 13:36:49 +0100

    Print progress

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #import libraries
 import urllib2
 import urllib
+import threading
+import sys
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup;
+soup = BeautifulSoup
+downloadCount = 0
 
 def getPageURLs():
     #test url
     url = 'http://pacifist-stephania.tumblr.com/'
+    print("Opening URL: " + url)
     page = urllib2.urlopen(url)
 
     #parse html
@@ -17,13 +21,14 @@ def getPageURLs():
     urls = []
     for tag in posts:
         try:
-            if tag.parent['class'][0] == 'post':
+            if "tumblr.com/post" in tag['href']:
                 urls.append(tag['href'])
         except Exception:
             pass
     return urls
 
 def getImageURLs(urls):
+    print("Retreiving Image URLs...")
     imgURLs = []
     for url in urls:
         page = urllib2.urlopen(url)
@@ -33,22 +38,36 @@ def getImageURLs(urls):
 
         for tag in allImages:
             try:
-                if tag.parent.parent['class'][1] == 'photo':
+                if "/tumblr_" in tag['src']:
                     imgURLs.append(tag['src'])
             except Exception:
                 pass
     return imgURLs
 
 def downloadImages(imgURLs):
-    for imgURL in imgURLs:
-        imgName = imgURL.rsplit('/', 1)[1];
-        urllib.urlretrieve(imgURL, imgName)
+    threads = [threading.Thread(target=downloadImage, args=(url,)) for url in imgURLs]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    print("\nDownloads Complete.")
+        
+def downloadImage(url):
+    imgName = url.rsplit('/', 1)[1]
+    urllib.urlretrieve(url, imgName)
+    onDownloadComplete()
 
+def onDownloadComplete():
+    global downloadCount
+    downloadCount = downloadCount + 1
+    message = "\rImages Downloaded: " + str(downloadCount)
+    sys.stdout.write(message)
+    sys.stdout.flush()
+   
 def main():
     urls = getPageURLs()
     imgURLs = getImageURLs(urls)
-    print(imgURLs)
     downloadImages(imgURLs)
-
+    
 if __name__ == "__main__": main()
 
