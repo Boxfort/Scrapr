54b23b80d4b2e8fb18727c5abded462a2ee04f7d
Boxfort <40208539@live.napier.ac.uk>
2017-09-02 14:22:56 +0100

    arguments and support for iri

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import urllib2
 import urllib
 import threading
 import sys
+import argparse
+import re, urlparse
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup
+
+parser = argparse.ArgumentParser(description='Download images from images from a tumblr.')
+parser.add_argument('name', help='The name of the tumblr.')
+parser.add_argument('--tags', nargs='*', dest='tags', help='Image must contain one or more of the supplied tags.')
+
 downloadCount = 0
+args = []
 
 def getPageURLs():
-    #test url
-    url = 'http://pacifist-stephania.tumblr.com/'
+    url = 'http://'+str(args['name'])+'.tumblr.com/'
     print("Opening URL: " + url)
     page = urllib2.urlopen(url)
 
@@ -31,14 +38,14 @@ def getImageURLs(urls):
     print("Retreiving Image URLs...")
     imgURLs = []
     for url in urls:
-        page = urllib2.urlopen(url)
+        page = urllib2.urlopen(iriToUri(url))
         html = soup(page, 'html.parser')
 
         allImages = html.findAll('img')
 
         for tag in allImages:
             try:
-                if "/tumblr_" in tag['src']:
+                if "/tumblr_" in tag['src'] and "/tumblr_static" not in tag['src']:
                     imgURLs.append(tag['src'])
             except Exception:
                 pass
@@ -63,8 +70,22 @@ def onDownloadComplete():
     message = "\rImages Downloaded: " + str(downloadCount)
     sys.stdout.write(message)
     sys.stdout.flush()
+
+def urlEncodeNonAscii(b):
+    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
+
+def iriToUri(iri):
+    parts= urlparse.urlparse(iri)
+    return urlparse.urlunparse(
+        part.encode('idna') if parti==1 else urlEncodeNonAscii(part.encode('utf-8'))
+        for parti, part in enumerate(parts)
+    )
    
 def main():
+    global args
+    args = vars(parser.parse_args())
+    print(args)
+
     urls = getPageURLs()
     imgURLs = getImageURLs(urls)
     downloadImages(imgURLs)
