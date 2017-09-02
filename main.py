6f47bb7c1dc1e21128adb55ce2e1e87fbb8399f1
Boxfort <40208539@live.napier.ac.uk>
2017-09-02 12:00:01 +0100

    get image urls

--- main.py
+++ main.py
@@ -4,17 +4,45 @@ from bs4 import BeautifulSoup
 
 soup = BeautifulSoup;
 
-#test url
-url = 'http://pacifist-stephania.tumblr.com/'
-page = urllib2.urlopen(url)
 
-#parse html
-html = soup(page, 'html.parser')
-posts = html.findAll('a')
+def getPageURLs():
+    #test url
+    url = 'http://pacifist-stephania.tumblr.com/'
+    page = urllib2.urlopen(url)
 
-for tag in posts:
-    try:
-        if tag.parent['class'][0] == 'post':
-            print (tag['href'])
-    except Exception:
-        print("Parent has no class.")
+    #parse html
+    html = soup(page, 'html.parser')
+    posts = html.findAll('a')
+
+    urls = []
+    for tag in posts:
+        try:
+            if tag.parent['class'][0] == 'post':
+                urls.append(tag['href'])
+        except Exception:
+            pass
+    return urls
+
+def getImageURLs(urls):
+    imgURLs = []
+    for url in urls:
+        page = urllib2.urlopen(url)
+        html = soup(page, 'html.parser')
+
+        allImages = html.findAll('img')
+
+        for tag in allImages:
+            try:
+                if tag.parent.parent['class'][1] == 'photo':
+                    imgURLs.append(tag['src'])
+            except Exception:
+                pass
+    return imgURLs
+
+def main():
+    urls = getPageURLs()
+    imgURLs = getImageURLs(urls)
+    print(imgURLs)
+
+if __name__ == "__main__": main()
+
