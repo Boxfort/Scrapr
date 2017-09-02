2983d84714ad86df58c505dc01e733461fd97cf6
Boxfort <40208539@live.napier.ac.uk>
2017-09-02 20:18:08 +0100

    can choose directory

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import threading
 import sys
 import argparse
 import re, urlparse
+import os
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup
@@ -15,8 +16,10 @@ parser.add_argument('name', help='Name of blog')
 parser.add_argument('-t', nargs=1, dest='tag', help='Download images with specified tag.')
 parser.add_argument('-p', nargs=1, dest='page', type=int, default=[1], help='Page to start on.')
 parser.add_argument('-n', nargs=1, dest='pages', type=int, default=[1], help='Number of pages to scrape.')
+parser.add_argument('-d', nargs=1, dest='directory', type=int, default=[os.getcwd()], help='Directory to save the images.')
 
 downloadCount = 0
+directory = None
 
 def getPageURLs(name, tag, page):
 
@@ -33,7 +36,6 @@ def getPageURLs(name, tag, page):
         page = urllib2.urlopen(url)
     except Exception:
         return None
-
 
     #parse html
     html = soup(page, 'html.parser')
@@ -62,7 +64,7 @@ def getImageURLs(urls):
 
             for tag in allImages:
                 try:
-                    if "/tumblr_" in tag['src'] and "/tumblr_static" not in tag['src'] and tag['src'] not in imgURLs:
+                    if "68.media" in tag['src'] and "/tumblr_static" not in tag['src'] and "avatar" not in tag['src'] and tag['src'] not in imgURLs:
                         imgURLs.append(tag['src'])
                         print(tag['src'])
                 except Exception:
@@ -79,11 +81,18 @@ def downloadImages(imgURLs):
     for thread in threads:
         thread.join()
     print("\nDownloads Complete.")
-        
+
 def downloadImage(url):
     imgName = url.rsplit('/', 1)[1]
-    urllib.urlretrieve(url, imgName)
-    onDownloadComplete()
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(os.path.join(directory))
+
+        urllib.urlretrieve(url, os.path.join(directory, imgName))
+        onDownloadComplete()
+    except Exception as e:
+        print(str(e))
+        exit()
 
 def onDownloadComplete():
     global downloadCount
@@ -103,15 +112,16 @@ def iriToUri(iri):
     )
    
 def main():
+    global directory
     args = vars(parser.parse_args())
-    print(args)
+    directory = os.path.join(args['directory'][0], args['name'])
 
     for i in range(args['page'][0], args['page'][0] + args['pages'][0]):
         urls = getPageURLs(args['name'], args['tag'], i)
         if not urls:
+            print("No images found.")
             exit()
         imgURLs = getImageURLs(urls)
         downloadImages(imgURLs)
     
 if __name__ == "__main__": main()
-
